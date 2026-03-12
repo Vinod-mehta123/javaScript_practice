2c06592048c7d9582cd842bf59cd428e9a187a35
Vinod Mehta <134594051+Vinod-mehta123@users.noreply.github.com>
2026-03-12 10:41:22 +0000

    object traverse

--- 01_basics/04_map.js
+++ 01_basics/04_map.js
@@ -1,3 +1,4 @@
+/*
 const map = new Map();
 map.set('IN', "India");
 map.set('SL', "Srilanka")
@@ -7,4 +8,17 @@ console.log(map);
 
 for(const [key, value] of map){
     console.log(`Here key is country code ${key} and value is country name ${value}`);
+}
+*/
+// if it was a object then traversing would be different
+
+let my_object = new Object;
+my_object = {
+    js: 'javaScript',
+    cpp: 'C plus plus',
+    swift : 'swift by apple',
+}
+
+for(const key in my_object){
+    console.log(`${key} is shortcut for ${my_object[key]}`);
 }
